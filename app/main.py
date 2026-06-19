e1c8e35742c8c781888de3f45f470d3bb85fdc6f
karthik <chukkakarthik1506@gmail.com>
2026-06-19 20:58:49 +0530

    connected fastapi to news_service api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.services.news_service import get_news
 
 app = FastAPI()
 
@@ -7,3 +7,10 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {'message':'News Summarizer'}
+
+
+@app.get('/news')
+async def news(topic: str):
+    data = await get_news(topic)
+
+    return data
--- app/services/news_service.py
+++ app/services/news_service.py
@@ -1,7 +1,22 @@
 from dotenv import load_dotenv
+import httpx
 import os
 
 load_dotenv()
 
-api_key = os.getenv('News_api_key')
+API_KEY = os.getenv('News_api_key')
 
+async def get_news(topic : str):
+    url = "https://newsapi.org/v2/everything"
+
+    # Query parameters.
+    params = {
+        'q': topic,
+        'apikey':API_KEY
+    }
+    async with httpx.AsyncClient() as client:
+        # actual API call.
+        response = await client.get(url,params=params)
+
+    # Converts JSON into a Python dictionary.
+    return response.json()
